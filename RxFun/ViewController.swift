09f774dbeec06016591e374a25d17c15c92cbab6
Jian Ma <jianquanma@gmail.com>
2020-01-19 18:23:48 -0500

    Initial Commit

---
+++ RxFun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxFun
+//
+//  Created by Jian Ma on 1/19/20.
+//  Copyright © 2020 Jian Ma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
